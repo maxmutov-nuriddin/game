99516be0f538491ff77302d5c4d64aa4d7e21a3b
maxmutov-nuriddin <mahmudovnuriddin35@gmail.com>
2023-04-15 23:54:11 +0500

    first commit

---
+++ js/main.js
@@ -0,0 +1,24 @@
+let click = document.querySelector('.game__dino')
+let cactus = document.querySelector('.game__cactus')
+
+document.addEventListener('keydown', (e) => {
+  run()
+})
+
+function run() {
+  if (click.classList != "jump") {
+    click.classList.add('jump')
+  } setTimeout(() => {
+    click.classList.remove('jump')
+  }, 550);
+}
+
+let bang = setInterval(function () {
+  let clickTop = parseInt(window.getComputedStyle(click).getPropertyValue("top"))
+  let cactusLeft = parseInt(window.getComputedStyle(cactus).getPropertyValue("left"))
+
+
+  if (cactusLeft < 50 && cactusLeft > 0 && clickTop >= 253) {
+    alert("GAME OVER!!")
+  }
+}, 10)
